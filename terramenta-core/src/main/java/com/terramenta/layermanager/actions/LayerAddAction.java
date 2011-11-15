f700252159f01db7656c3b55c467ee2a7e8edc04
heidtmare <none@none>
2011-11-15 17:22:29 -0500

    TM-19

--- terramenta-core/src/main/java/com/terramenta/layermanager/actions/LayerAddAction.java
+++ terramenta-core/src/main/java/com/terramenta/layermanager/actions/LayerAddAction.java
@@ -1,17 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in the editor.
  */
 package com.terramenta.layermanager.actions;
 
 import com.terramenta.globe.WorldWindManager;
-import com.terramenta.layermanager.LayerSelectorPanel;
+import com.terramenta.globe.layers.KMLLayer;
+import com.terramenta.layermanager.layerselector.LayerSelectorWizardIterator;
+import gov.nasa.worldwind.layers.Layer;
+import gov.nasa.worldwind.layers.SurfaceImageLayer;
+import gov.nasa.worldwindx.examples.util.ShapefileLoader;
+import java.awt.Dialog;
 import java.awt.event.ActionEvent;
-import org.openide.DialogDescriptor;
+import java.io.IOException;
+import java.net.URL;
+import java.text.MessageFormat;
 import org.openide.DialogDisplayer;
-import org.openide.NotifyDescriptor;
+import org.openide.WizardDescriptor;
 import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.HelpCtx;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
@@ -21,42 +29,66 @@ import org.openide.util.actions.SystemAction;
  *
  * @author heidtmare
  */
-@ActionID(category = "Other", id = "com.terramenta.layermanager.actions.LayerAddAction")
+@ActionID(category = "Tools", id = "com.terramenta.layermanager.actions.LayerAddAction")
 @ActionRegistration(displayName = "#CTL_LayerAddAction")
 @Messages("CTL_LayerAddAction=Add Layer")
+@ActionReference(path = "Menu/Tools")
 public class LayerAddAction extends SystemAction {
 
-    private static final WorldWindManager wwm = Lookup.getDefault().lookup(WorldWindManager.class);
-
-    /**
-     *
-     * @param evt
-     */
     @Override
-    public void actionPerformed(ActionEvent evt) {
-        LayerSelectorPanel ls = new LayerSelectorPanel();
-        DialogDescriptor d = new DialogDescriptor(ls, "Add Layer");
-        Object result = DialogDisplayer.getDefault().notify(d);
-        //was cancel hit?
-        if (result != NotifyDescriptor.OK_OPTION) {
-            return;
+    public void actionPerformed(ActionEvent e) {
+        LayerSelectorWizardIterator iterator = new LayerSelectorWizardIterator();
+        WizardDescriptor wizardDescriptor = new WizardDescriptor(iterator);
+        iterator.initialize(wizardDescriptor);
+        //{0} will be replaced by WizardDescriptor.Panel.getComponent().getName()
+        // {1} will be replaced by WizardDescriptor.Iterator.name()
+        wizardDescriptor.setTitleFormat(new MessageFormat("{0} ({1})"));
+        wizardDescriptor.setTitle("Layer Selector");
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(wizardDescriptor);
+        dialog.setVisible(true);
+        dialog.toFront();
+        boolean cancelled = wizardDescriptor.getValue() != WizardDescriptor.FINISH_OPTION;
+        if (!cancelled) {
+            Object layerType = wizardDescriptor.getProperty("layerType");
+            Object layerPath = wizardDescriptor.getProperty("layerPath");
+            if (layerType != null && layerPath != null) {
+                Layer layer = null;
+
+                try {
+                    if (layerType.equals("KML")) {
+                        layer = new KMLLayer((String) layerPath);
+                    } else if (layerType.equals("Shapefile")) {
+                        ShapefileLoader loader = new ShapefileLoader();
+                        layer = (Layer) loader.createLayersFromSource(new URL((String) layerPath));
+                    } else if (layerType.equals("GeoTiff")) {
+                        SurfaceImageLayer sil = new SurfaceImageLayer();
+                        sil.addImage((String) layerPath);
+                        layer = sil;
+                    }
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+
+                if (layer != null) {
+                    Lookup.getDefault().lookup(WorldWindManager.class).getLayers().add(layer);
+                }
+            }
         }
-        wwm.getWorldWindow().getModel().getLayers().add(ls.getLayer());
     }
 
     /**
-     *
-     * @return
-     */
+         *
+         * @return
+         */
     @Override
     public String getName() {
         return Bundle.CTL_LayerAddAction();
     }
 
     /**
-     *
-     * @return
-     */
+         *
+         * @return
+         */
     @Override
     public HelpCtx getHelpCtx() {
         return HelpCtx.DEFAULT_HELP;
---
+++ terramenta-core/src/main/java/com/terramenta/layermanager/layerselector/LayerSelectorWizardIterator.java
@@ -0,0 +1,145 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in the editor.
+ */
+package com.terramenta.layermanager.layerselector;
+
+import java.awt.Component;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import javax.swing.JComponent;
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.WizardDescriptor.Panel;
+
+public final class LayerSelectorWizardIterator implements WizardDescriptor.Iterator {
+
+    // To invoke this wizard, copy-paste and run the following code, e.g. from
+    // SomeAction.performAction():
+    /*
+     * WizardDescriptor.Iterator iterator = new LayerSelectorWizardIterator(); WizardDescriptor wizardDescriptor = new WizardDescriptor(iterator); //
+     * {0} will be replaced by WizardDescriptor.Panel.getComponent().getName() // {1} will be replaced by WizardDescriptor.Iterator.name()
+     * wizardDescriptor.setTitleFormat(new MessageFormat("{0} ({1})")); wizardDescriptor.setTitle("Your wizard dialog title here"); Dialog dialog =
+     * DialogDisplayer.getDefault().createDialog(wizardDescriptor); dialog.setVisible(true); dialog.toFront(); boolean cancelled =
+     * wizardDescriptor.getValue() != WizardDescriptor.FINISH_OPTION; if (!cancelled) { // do something }
+     */
+    private int index;
+    private WizardDescriptor wizardDesc;
+    private HashMap<String, String[]> labels;
+    private HashMap<String, Panel[]> sequences;
+    private String layerType = "WMS";
+
+    public void initialize(WizardDescriptor wizardDescriptor) {
+        wizardDesc = wizardDescriptor;
+    }
+
+    /**
+     * Initialize panels representing individual wizard's steps and sets
+     * various properties for them influencing wizard appearance.
+     */
+    private WizardDescriptor.Panel[] getPanels() {
+        if (sequences == null) {
+            WizardDescriptor.Panel[] allPanels = new WizardDescriptor.Panel[]{
+                new LayerSelectorLayerType(),
+                new LayerSelectorGeotiff(),
+                new LayerSelectorKml(),
+                new LayerSelectorShapefile(),
+                new LayerSelectorWms()
+            };
+            String[] steps = new String[allPanels.length];
+            for (int i = 0; i < allPanels.length; i++) {
+                Component c = allPanels[i].getComponent();
+                // Default step name to component name of panel.
+                steps[i] = c.getName();
+                if (c instanceof JComponent) { // assume Swing components
+                    JComponent jc = (JComponent) c;
+                    // Sets step number of a component
+                    // TODO if using org.openide.dialogs >= 7.8, can use WizardDescriptor.PROP_*:
+                    jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i));
+                    // Sets steps names for a panel
+                    jc.putClientProperty("WizardPanel_contentData", steps);
+                    // Turn on subtitle creation on each step
+                    jc.putClientProperty("WizardPanel_autoWizardStyle", Boolean.TRUE);
+                    // Show steps on the left side with the image on the background
+                    jc.putClientProperty("WizardPanel_contentDisplayed", Boolean.TRUE);
+                    // Turn on numbering of all steps
+                    jc.putClientProperty("WizardPanel_contentNumbered", Boolean.TRUE);
+                }
+            }
+
+            labels = new HashMap<String, String[]>();
+            labels.put("GEOTIFF", new String[]{steps[0], steps[1]});
+            labels.put("KML", new String[]{steps[0], steps[2]});
+            labels.put("SHAPEFILE", new String[]{steps[0], steps[3]});
+            labels.put("WMS", new String[]{steps[0], steps[4]});
+
+            sequences = new HashMap<String, WizardDescriptor.Panel[]>();
+            sequences.put("GEOTIFF", new WizardDescriptor.Panel[]{allPanels[0], allPanels[1]});
+            sequences.put("KML", new WizardDescriptor.Panel[]{allPanels[0], allPanels[2]});
+            sequences.put("SHAPEFILE", new WizardDescriptor.Panel[]{allPanels[0], allPanels[3]});
+            sequences.put("WMS", new WizardDescriptor.Panel[]{allPanels[0], allPanels[4]});
+
+            setLayerType(layerType);
+        }
+        return sequences.get(layerType);
+    }
+
+    private void setLayerType(String layerType) {
+        this.layerType = layerType;
+        wizardDesc.putProperty(WizardDescriptor.PROP_CONTENT_DATA, labels.get(layerType));
+    }
+
+    public WizardDescriptor.Panel current() {
+        return getPanels()[index];
+    }
+
+    public String name() {
+        return index + 1 + " of " + getPanels().length;
+    }
+
+    public boolean hasNext() {
+        return index < getPanels().length - 1;
+    }
+
+    public boolean hasPrevious() {
+        return index > 0;
+    }
+
+    public void nextPanel() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (index == 0) {
+            Object lt = wizardDesc.getProperty("layerType");
+            if (lt != null) {
+                setLayerType((String) lt);
+            }
+        }
+        index++;
+        wizardDesc.putProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, index);
+    }
+
+    public void previousPanel() {
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
+        index--;
+        wizardDesc.putProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, index);
+    }
+
+    // If nothing unusual changes in the middle of the wizard, simply:
+    public void addChangeListener(ChangeListener l) {
+    }
+
+    public void removeChangeListener(ChangeListener l) {
+    }
+    // If something changes dynamically (besides moving between panels), e.g.
+    // the number of panels changes in response to user input, then uncomment
+    // the following and call when needed: fireChangeEvent();
+    /*
+     * private Set<ChangeListener> listeners = new HashSet<ChangeListener>(1); // or can use ChangeSupport in NB 6.0 public final void
+     * addChangeListener(ChangeListener l) { synchronized (listeners) { listeners.add(l); } } public final void removeChangeListener(ChangeListener l)
+     * { synchronized (listeners) { listeners.remove(l); } } protected final void fireChangeEvent() { Iterator<ChangeListener> it; synchronized
+     * (listeners) { it = new HashSet<ChangeListener>(listeners).iterator(); } ChangeEvent ev = new ChangeEvent(this); while (it.hasNext()) {
+     * it.next().stateChanged(ev); } }
+     */
+}
