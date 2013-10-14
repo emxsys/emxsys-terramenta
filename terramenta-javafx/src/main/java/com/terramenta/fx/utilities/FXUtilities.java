6b4fbeddfc520e469e0eff41c95b8ab463400387
heidtmare <none@none>
2013-10-14 17:11:06 -0400

    cleaning up some misplaced resources

--- terramenta-javafx/src/main/java/com/terramenta/fx/utilities/FXUtilities.java
+++ terramenta-javafx/src/main/java/com/terramenta/fx/utilities/FXUtilities.java
@@ -6,6 +6,7 @@ package com.terramenta.fx.utilities;
 
 import java.io.IOException;
 import java.net.URL;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,6 +16,14 @@ import javafx.scene.Scene;
  * @author Chris.Heidt
  */
 public class FXUtilities {
+
+    public static void run(Runnable run) {
+        if (Platform.isFxApplicationThread()) {
+            run.run();
+        } else {
+            Platform.runLater(run);
+        }
+    }
 
     public static Scene createScene(URL fxml, Object controller, String stylesheet) throws IOException {
         if (fxml == null) {
