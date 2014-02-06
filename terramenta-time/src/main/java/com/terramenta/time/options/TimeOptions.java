113c0abcdd3e05896f17a02910ef00b5709f15c2
heidtmare <none@none>
2014-02-06 12:50:54 -0500

    Fixed #13: Added Time Options and updated the animation datetime control

---
+++ terramenta-time/src/main/java/com/terramenta/time/options/TimeOptions.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.terramenta.time.options;
+
+/**
+ *
+ * @author Chris.Heidt
+ */
+public class TimeOptions {
+    public static final String TIMEZONE = "tm.time.timezone";
+    public static final String LOCALE = "tm.time.locale";
+    
+    public static final String DEFAULT_TIMEZONE = "UTC";
+    public static final String DEFAULT_LOCALE = "en_US";
+}
--- terramenta-time/src/main/java/com/terramenta/time/ribbons/DatetimeBand.java
+++ terramenta-time/src/main/java/com/terramenta/time/ribbons/DatetimeBand.java
@@ -6,17 +6,21 @@ package com.terramenta.time.ribbons;
 
 import com.terramenta.time.DateProvider;
 import com.terramenta.time.datepicker.DatePicker;
+import com.terramenta.time.options.TimeOptions;
 import java.awt.Dimension;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.TimeZone;
+import java.util.prefs.Preferences;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import org.openide.util.Lookup;
+import org.openide.util.NbPreferences;
 import org.pushingpixels.flamingo.api.ribbon.JFlowRibbonBand;
 
 /**
@@ -26,6 +30,7 @@ import org.pushingpixels.flamingo.api.ribbon.JFlowRibbonBand;
 public class DatetimeBand extends JFlowRibbonBand implements Observer {
 
     private static final DateProvider dateProvider = Lookup.getDefault().lookup(DateProvider.class);
+    private static final Preferences prefs = NbPreferences.forModule(TimeOptions.class);
     private DatePicker picker;
 
     /**
@@ -35,7 +40,7 @@ public class DatetimeBand extends JFlowRibbonBand implements Observer {
         super("Datetime", null);
 
         final JFXPanel jfxPanel = new JFXPanel();
-        jfxPanel.setPreferredSize(new Dimension(150, 48));
+        jfxPanel.setPreferredSize(new Dimension(220, 48));
         Platform.setImplicitExit(false);
         Platform.runLater(new Runnable() {
 
@@ -45,14 +50,18 @@ public class DatetimeBand extends JFlowRibbonBand implements Observer {
             }
         });
 
-        setPreferredSize(new Dimension(180, 60));
+        setPreferredSize(new Dimension(220, 60));
         addFlowComponent(jfxPanel);
 
         dateProvider.addObserver(this);
     }
 
     private Scene createScene() {
-        picker = new DatePicker("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone("UTC"));
+        picker = new DatePicker(
+                "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+                TimeZone.getTimeZone(prefs.get(TimeOptions.TIMEZONE, TimeOptions.DEFAULT_TIMEZONE)),
+                Locale.forLanguageTag(prefs.get(TimeOptions.LOCALE, TimeOptions.DEFAULT_LOCALE))
+        );
         picker.setDate(dateProvider.getDate());
 
         picker.timestampProperty().addListener(new ChangeListener<Long>() {
